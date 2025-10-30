232e86aa1f9d67a01d420aad9067502a9e0f34d0
DavidAdam1323 <davidadamgtr@gmail.com>
2025-10-30 02:19:31 +0000

    Add all questions including license dropdown.

--- main.py
+++ main.py
@@ -5,9 +5,15 @@ def main():
   question_manager.display_welcome()
   
   questions = question_manager.get_basic_questions()
-  print("Questions to be used:", len(questions))
-  print("First Question:", questions[0]["message"])
-  print("Second Question:", questions[1]["message"])
+  print(f"Ready to ask {len(questions)} questions:\n")
+  
+  for i, question in enumerate(questions, 1):
+    print(f"{i}. {question["message"]}")
+    
+  print(f"\nNotice: The license question has {len(questions[4]["choices"])} options to choose from!")
+  
+  # print("First Question:", questions[0]["message"])
+  # print("Second Question:", questions[1]["message"])
   
   # print("Welcome to README generator!")
   # print("Testing...")
--- question_manager.py
+++ question_manager.py
@@ -7,13 +7,46 @@ class QuestionManager:
       {
         "type": "input",
         "name": "title",
-        "message": "What is your project title?"
+        "message": "What is your project title?",
+        "validate": lambda answer: "Please, enter your project title." if len(answer) == 0 else True
       },
       {
         "type": "input",
         "name": "description",
-        "message": "Write a short description of your project:"
-      }
+        "message": "Write a short description of your project:",
+        "validate": lambda answer: "Please, enter a description." if len(answer) == 0 else True
+      },
+      {
+        "type": "input",
+        "name": "installation",
+        "message": "What are the installation instructions?"
+      },
+      {
+        "type": "input",
+        "name": "usage",
+        "message": "How do you use this project?"
+      },
+      {
+        "type": "list",
+        "name": "license",
+        "message": "Choose a license for your project:",
+        "choices": ["MIT License", "Unlicense", "No license", "None"]
+      },
+      {
+        "type": "input",
+        "name": "author",
+        "message": "What is your name (Author)?"
+      },
+      {
+        "type": "input",
+        "name": "email",
+        "message": "What is your email address?"
+      },
+      {
+        "type": "input",
+        "name": "linkedin",
+        "message": "What is your LinkedIn profile URL?"
+      },
     ]
     
     return basic_questions
